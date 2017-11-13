20dedbc709c825419f03fb6918adeb13e9b0ecad
Argador <steven.lindblom@gmail.com>
2017-11-13 17:05:19 -0500

    TestingSet update
    Added number of each genre into values to test/make scatterplots out of

--- TestingSet.py
+++ TestingSet.py
@@ -8,6 +8,11 @@ data = pd.read_csv('GameSales2016.csv', names = ColNames, skiprows = 1)
 # There are 2 games without genres, I don’t know if we should add a 13th category unknown, just so that we aren’t losing anything in the dataset and prevent problems with the analysis.
 #{Action = 0, Adventure = 1, Fighting = 2, Misc = 3, Platform = 4, Puzzle = 5, Racing = 6, Role-Playing = 7, Shooter = 8, Simulation = 9, Sports = 10, Strategy = 11}
 
+#Num of Games in each genre, useful for train/test later
+#Action = 3370, Adventure = 1303, Fighting = 849, Misc = 1750, Platform = 888
+#Puzzle = 580, Racing = 1249, Role-Playing = 1500, Shooter = 1323
+#Simulation = 874, Sports = 2348, Strategy = 683
+
 #Calling Specific Columns
 #CriticScores = data.Critic_Scores.tolist()
 GlobalSales = data.Global_Sales.tolist()
@@ -18,6 +23,18 @@ GlobalSales = data.Global_Sales.tolist()
 #plt.show()
 
 genre = data.genre.tolist()
+Action = genre[:3370]
+Adventure = genre[3370:4673]
+Fighting = genre[4673:5522]
+Misc = genre[5522:7272]
+Platform = genre[7272:8160]
+Puzzle = genre[8160:8740]
+Racing = genre[8740:9989]
+Role-Playing = genre[9989:11489]
+Shooter = genre[11489:12812]
+Simulation = genre[12812:13686]
+Sports = genre[13686:16034]
+Strategy = genre[16034:16717]
 plt.scatter(genre, GlobalSales, color = 'r')
 plt.xlabel('Genre')
 plt.ylabel('Global Sales')
